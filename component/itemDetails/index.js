91265c5967cd21418c3417e403588f0407058f05
ray <wz8031@mun.ca>
2021-04-13 14:58:11 -0230

    first commit

---
+++ component/itemDetails/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, Image, Pressable, } from 'react-native';
+
+const itemDetails = () =>{
+    return(
+        <View>
+            <Text>this is a detail page</Text>
+        </View>
+    )
+}
+
+export default itemDetails;
