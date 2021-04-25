9d91068777998c9cf3660909a64f0be28e47bdef
ray <wz8031@mun.ca>
2021-04-25 13:26:35 -0230

    update bottom tab bar to matirial tab bar

--- component/BottomNavigator/index.js
+++ component/BottomNavigator/index.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import {View} from 'react-native';
-import HomeNavigator from '../HomeNavigation';
+import HomeNavigator from '../Home/HomeNavigation';
 import itemDetails from '../itemDetails';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomNavigator = ()=>{
     return(
         <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeNavigator} />
-        <Tab.Screen name="Details" component={itemDetails} />
-        <Tab.Screen name="Favorite" component={itemDetails} />
-        <Tab.Screen name="Cart" component={itemDetails} />
+        <Tab.Screen name="Home" component={HomeNavigator} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" color={color} size={26} />
+          ),
+        }}/>
+
+        <Tab.Screen name="Order" component={itemDetails} options={{
+          tabBarLabel: 'Order',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="hourglass-sharp" color={color} size={26} /> ),
+        }}/>
+
+        <Tab.Screen name="Scan" component={itemDetails} options={{
+          tabBarLabel: 'Scan',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="scan-sharp" color={color} size={26} /> ),
+        }}/>
+
+        <Tab.Screen name="Favorite" component={itemDetails} options={{
+          tabBarLabel: 'Favorite',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="heart-sharp" color={color} size={26} /> ),
+        }}/>
+
+        <Tab.Screen name="Cart" component={itemDetails} options={{
+          tabBarLabel: 'Cart',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="cart-sharp" color={color} size={26}/>),
+          }}/>
 
 
       </Tab.Navigator>
--- component/Home/HomeNavigation/index.js
+++ component/Home/HomeNavigation/index.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import Home from '../Home'
-import itemDetails from '../itemDetails';
+import Home from '../../Home'
+import itemDetails from '../../itemDetails';
 // import BottomNavigator from '../BottomNavigator';
 
 const Stack = createStackNavigator();
